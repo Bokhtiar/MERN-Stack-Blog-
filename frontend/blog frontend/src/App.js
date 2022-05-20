819a9ac8882cf6144f7b303678cf3895c7d6befd
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-05-20 12:56:28 +0600

    frontend Navbar deisng

--- frontend/blog frontend/src/App.js
+++ frontend/blog frontend/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Header from './Components/Layouts/Header';
+import Footer from './Components/Layouts/Footer';
+import Home from "./Components/Index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <BrowserRouter>    
+          <Header/>
+          <Routes>
+              <Route path="/" element={<Home />}></Route>
+              {/* <Route path="/blog" element={<BlogList/>}></Route>
+              <Route path="/blog/detail/:id" element={ <SingleBlog /> }></Route>
+              <Route path="/about" element={ <About  /> }></Route>
+              <Route path="/resume" element={ <Resume /> }></Route>
+              <Route path="/project" element={ <Project  /> }></Route> */}
+            </Routes>
+          <Footer/>
+        </BrowserRouter>    
     </div>
   );
 }
