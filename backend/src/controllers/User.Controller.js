93ead5c9739da2c789eae0d0791902a146bcb211
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-05-21 00:16:43 +0600

    backend user list and user store done

--- backend/src/controllers/User.Controller.js
+++ backend/src/controllers/User.Controller.js
@@ -2,17 +2,55 @@ const user = require('../models/user.model')
 
 //list of user
 const Index = async(req, res, next) => {
-    res.status(200).json({
-        status:true,
-        message : "ok",
-    })
+    try {
+        const results = await user.find()
+
+        res.status(200).json({
+            status:true,
+            data: results,
+            message: "list of users"
+        })
+    } catch (error) {
+        if(error){
+            console.log('error', error)
+            next(error)
+        }   
+    }
 }
 
 //store user
+const Store = async (req, res, next) => {
+    try {
+        const {
+            name,
+            email,
+            password,
+            role
+        } = req.body
 
+        const newUser = new user({
+            name,
+            email,
+            password,
+            role
+        })
 
+        await newUser.save()
+        res.status(200).json({
+            status:true,
+            message: "Login Successfully"
+        })
+
+    } catch (error) {
+        if (error) {
+            console.log('error', error)
+            next(error)
+        }
+    }
+}
 
 
 module.exports = {
     Index,
+    Store
 }
