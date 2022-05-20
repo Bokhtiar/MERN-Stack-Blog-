8e9031b7379c122b48c091b839bb30f8238899f4
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-05-21 02:38:43 +0600

    frontend login successfully done

--- backend/src/controllers/Auth.Controller.js
+++ backend/src/controllers/Auth.Controller.js
@@ -7,12 +7,25 @@ const Login = async (req, res, next)=> {
             password
         } = req.body
 
-        const AuthLogin = await user.where('email', email).where('password', password).find()
-        res.status(200).json({
-            status: true,
-            data: AuthLogin,
-            message: "Login Successfully...!"
-        })
+        if (!email || !password) {
+            return res.status(400).json({
+              message: "Username or Password not present",
+            })
+        }
+        const loginUser = await user.findOne({ email, password })
+        if(loginUser){
+            res.status(200).json({
+                status: true, 
+                data: loginUser,
+                message: "Login Successfully...!"
+            })
+        }else{
+            res.status(500).json({
+                status: false, 
+                message: "Enter Your Valid Password and E-mail...!"
+            })
+        }
+        
        
         
     } catch (error) {
--- frontend/blog frontend/src/Components/Auth/Login.jsx
+++ frontend/blog frontend/src/Components/Auth/Login.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { useState } from 'react'
 import {Form,Container,Button} from 'react-bootstrap'
+
+
 const Login = () => {
     const [user, setUser] = useState({
         'email' : '',
@@ -10,14 +12,22 @@ const Login = () => {
         e.persist();
         setUser({...user,[e.target.name]: e.target.value})
     }
-    const Login = (e) => {
+
+    const Login = (e,replace) => {
         e.preventDefault();
         axios.post('/login', {
             'email' : user.email,
-            'password' : user.passwrod,
+            'password' : user.password,
           })
           .then(function (response) {
-            console.log(response);
+            console.log(response.data.data.role)
+            if(response.data.data.role === 'user'){
+
+            }else{
+                
+            }
+            localStorage.setItem('email', response.data.user.email);
+            localStorage.setItem('password', response.data.user.password);
           })
           .catch(function (error) {
             console.log(error);
@@ -30,11 +40,11 @@ const Login = () => {
                 <Form onSubmit={Login}>
                     <Form.Group className="mb-3" controlId="formGroupEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control name='email' onClick={LoginHandle} value={user.email} type="email" placeholder="Enter email" />
+                        <Form.Control name='email' onChange={LoginHandle} value={user.email} type="email" placeholder="Enter email" />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control name='password' onClick={LoginHandle} value={user.password} type="password" placeholder="Password" />
+                        <Form.Control name='password' onChange={LoginHandle} value={user.password} type="password" placeholder="Password" />
                     </Form.Group>
                     <Button variant="primary" type="submit"> Login </Button>
                 </Form>
