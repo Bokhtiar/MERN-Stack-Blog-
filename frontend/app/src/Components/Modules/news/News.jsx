bda223eba9feb45ec8911d926784f8d4feee7e34
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-05-25 17:22:43 +0600

    news list show

--- frontend/app/src/Components/Modules/news/News.jsx
+++ frontend/app/src/Components/Modules/news/News.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react"
 import { useState } from "react"
 import { getNews } from "../../Service/News"
+import {Container, Row, Col, Card} from 'react-bootstrap'
+import { Link } from "react-router-dom"
 
 const News = () => {
     const [news, setNews] = useState([''])
@@ -16,7 +18,23 @@ const News = () => {
     return (
         
         <section>
-            news
+            <Container>
+                <Row>
+                {
+                    news.map((n, index)=>
+                    <Col className="my-3" sm={6} lg={3} md={3}>
+                            <Card style={{ width: '16rem' }}>
+                                <Card.Img style={{ height:'150px' }} variant="top" src={ n.image } />
+                                <Card.Body>
+                                    <Card.Title>{n.title}</Card.Title>
+                                    <Link to={`/blog/${n._id}`} variant="primary">view</Link>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )
+                }
+            </Row> 
+            </Container>
         </section>
     )
 }
