000d7d5e6dd863edcb5ee223e45b63f510350f2f
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-05-27 12:37:54 +0600

    user permission with api token check done

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,13 +6,14 @@ const ContactRoute = require('./Contact.route')
 const NewsRoute = require('./news.route')
 const AdminRoute = require('./admin.route')
 const adminPermission = require('../middleware/admin.permission.middleware')
+const userPermission = require('../middleware/user.permission.middleware')
 
 appRouter.use('/category', adminPermission.IsAdmin, CategoryRoute)
 appRouter.use('/blog', BlogRouter)
 appRouter.use('/user', UserRoute)
 appRouter.use('/admin', AdminRoute)
 appRouter.use('/contact', ContactRoute)
-appRouter.use('/news', NewsRoute)
+appRouter.use('/news', userPermission.IsUser, NewsRoute)
 
 
 module.exports = appRouter
